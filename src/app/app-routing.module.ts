4f146eb9e16460e8f914543bf9b9948d0a4e1b0c
Eteims <8teims@gmail.com>
2023-01-31 13:33:44 +0100

    Converting shopbot website to angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
+import { IndexPageComponent } from './index-page/index-page.component';
+import { PolicyPageComponent } from './policy-page/policy-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: IndexPageComponent},
+  {path: "about-us", component: AboutUsPageComponent},
+  {path: "policy", component: PolicyPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
