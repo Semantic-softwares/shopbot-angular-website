46f9c490fae2bdd79857b3b3753fbae66eaec715
alexonozor <alexonozor@gmail.com>
2023-04-21 14:07:52 +0400

    change host url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiKey: 'AIzaSyBTzycQbUj9vWxnq8VgI7pzmYxdEo6t8to',
-    hostUrl: 'https://shopbot.ngrok.io'
+    hostUrl: 'https://shopbot-server.herokuapp.com'
 };
